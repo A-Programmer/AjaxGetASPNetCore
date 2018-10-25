666eacc4f46fe677f9239789a3563393179381ca
Kamran Sadin <MrSadin@Gmail.Com>
2018-10-25 09:15:24 +0330

    Init

---
+++ Project/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace Project.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public int Price { get; set; }
+    }
+}
